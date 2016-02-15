078fa4a01ea2bfc1d9b41e078ea9a0508d0b0803
Brian Conway <conwaybr@my.wccc.edu>
2016-02-15 10:48:58 -0500

    Update based on recommendations.

--- main.cpp
+++ main.cpp
@@ -31,65 +31,72 @@ int main(int argc, char *argv[]) {
 
         std::ofstream config_file;
 
-        string temp_value = "";
-
         config_file.open(CONFIG_FILE_PATH);
 
+
+        // Get the user's name
+        string name = "";
         cout << "Enter your name (first and last):" << endl;
-        std::getline(cin, temp_value);
+        std::getline(cin, name);
 
-        // If nothing was entered then ask again.
-        while (temp_value == "") {
+        while (name == "") {
             cout << "Error, please enter your name (first and last):" << endl;
-            std::getline(cin, temp_value);
+            std::getline(cin, name);
         }
-        config_file << "[name]=" << temp_value << endl;
-        temp_value = "";
+        config_file << "[name]=" << name << endl;
 
+
+        // Get the user's email address
+        string email = "";
         cout << "Enter your email address:" << endl; // TODO validate email address
-        std::getline(cin, temp_value);
+        std::getline(cin, email);
 
-        // If nothing was entered then ask again.
-        while (temp_value == "") {
+
+        while (email == "") {
             cout << "Error, please enter your email address:" << endl; // TODO validate email address
-            std::getline(cin, temp_value);
+            std::getline(cin, email);
         }
-        config_file << "[email]=" << temp_value << endl;
-        temp_value = "";
+        config_file << "[email]=" << email << endl;
 
+
+        // Get the user's password
+        string password = "";
         cout << "Enter your unique cypher:" << endl;
-        std::getline(cin, temp_value);
+        std::getline(cin, password);
 
         // If nothing was entered then ask again.
-        while (temp_value == "") {
+        while (password == "") {
             cout << "Error, please enter your unique cypher:" << endl;
-            std::getline(cin, temp_value);
+            std::getline(cin, password);
         }
-        config_file << "[cypher]=" << temp_value << endl;
-        temp_value = "";
+        config_file << "[cypher]=" << password << endl;
 
-        cout << "Enter your timezone-offset:" << endl;
-        std::getline(cin, temp_value);
+
+        // Get the user's timezone
+        string timezone_offset = "";
+        cout << "Enter your timezone-offset (example -5:00):" << endl;
+        std::getline(cin, timezone_offset);
 
         // If nothing was entered then ask again.
-        while (temp_value == "") {
-            cout << "Error, please enter your timezone-offset:" << endl;
-            std::getline(cin, temp_value);
+        while (timezone_offset == "") {
+            cout << "Error, please enter your timezone-offset (example -5:00):" << endl;
+            std::getline(cin, timezone_offset);
         }
-        config_file << "[timezone]=" << temp_value << endl;
-        temp_value = "";
+        config_file << "[timezone]=" << timezone_offset << endl;
 
-        cout << "Enter path to knownrecipients file:" << endl;
-        std::getline(cin, temp_value);
+
+        // Get the name of the knownrecipients file
+        string knownrecipients = "";
+        cout << "Enter path to knownrecipients file (hit enter for the default knownrecipients.txt):" << endl;
+        std::getline(cin, knownrecipients);
 
         // Set a default value if nothing was entered.
-        if (temp_value == "") {
+        if (knownrecipients == "") {
             config_file << "[knownrecipients]=./knownrecipients.txt" << endl;
         } else {
-            config_file << "[knownrecipients]=" << temp_value << endl;
+            config_file << "[knownrecipients]=" << knownrecipients << endl;
         }
 
-        temp_value = "";
 
         config_file.close();
 
@@ -98,14 +105,15 @@ int main(int argc, char *argv[]) {
         // edit the config file
 
         // Create a backup of the config file
-        const char * orig_file = CONFIG_FILE_PATH.c_str();
-        const char * backup_file = (CONFIG_FILE_PATH + ".old").c_str();
+
+        const string FILE = CONFIG_FILE_PATH;
+        const string BACKUP_FILE = FILE + ".backup";
 
         // Delete the previous backup if it exists
-        std::remove(backup_file);
+        std::remove(BACKUP_FILE.c_str());
 
         // Create a backup of the config before it is edited
-        int result = std::rename(orig_file, backup_file);
+        int result = std::rename(FILE.c_str(), BACKUP_FILE.c_str());
 
         if (result == 0) {
 
@@ -115,8 +123,8 @@ int main(int argc, char *argv[]) {
             std::ifstream infile;
             std::ofstream outfile;
 
-            infile.open(backup_file);
-            outfile.open(orig_file);
+            infile.open(BACKUP_FILE);
+            outfile.open(FILE);
 
             // Check if the second argument is blank
             string command_argument = "";
@@ -139,13 +147,15 @@ int main(int argc, char *argv[]) {
                     bool found_first_bracket = false; // Make true when the first '[' is found
 
                     for (int i = 0; i < read_line.length(); i++) {
-                        if (found_first_bracket) {
+                        if(!found_first_bracket){
+                            if (read_line[i] == '[') {
+                                found_first_bracket = true;
+                            }
+                        } else {
                             // Check for the next "]"
                             if (read_line[i] == ']') {
-
                                 setting_name = temp_value;
                                 setting_value = read_line.substr(i + 2, read_line.length() - i + 2);
-
                                 found_first_bracket = false;
                                 temp_value = "";
                                 break;
@@ -154,19 +164,33 @@ int main(int argc, char *argv[]) {
                                 // to lower case for comparison.
                                 std::locale loc;
                                 temp_value = temp_value + std::tolower(read_line[i], loc);
-
-                            }
-                        } else {
-                            if (read_line[i] == '[') {
-                                found_first_bracket = true;
                             }
                         }
+                }
 
+                // If no setting value is set then prompt for each line
+                if (command_argument == "") {
+
+                    cout << "The current setting for " << setting_name << " is: ";
+                    cout << setting_value << "." << endl;
+                    cout << "Please enter the new value ";
+                    cout << "or press enter to keep the ";
+                    cout << "current value." << endl;
+                    std::getline(cin, new_setting);
+                    if (new_setting == "") {
+                        outfile << read_line << endl;
+                    } else {
+                        outfile << "[" << setting_name << "]" << "=" << new_setting << endl;
                     }
+                    current_setting = "";
+                    new_setting + "";
 
-                    // If no setting value is set then prompt for each line
-                    if (command_argument == "") {
+                } else {
 
+                    // If the entered argument matches the current setting then
+                    // allow it to be updated else just save the current line
+                    // to the new file.
+                    if (command_argument == setting_name) {
                         cout << "The current setting for " << setting_name << " is: ";
                         cout << setting_value << "." << endl;
                         cout << "Please enter the new value ";
@@ -182,47 +206,33 @@ int main(int argc, char *argv[]) {
                         new_setting + "";
 
                     } else {
-
-                        // If the entered argument matches the current setting then
-                        // allow it to be updated else just save the current line
-                        // to the new file.
-                        if (command_argument == setting_name) {
-                            cout << "The current setting for " << setting_name << " is: ";
-                            cout << setting_value << "." << endl;
-                            cout << "Please enter the new value ";
-                            cout << "or press enter to keep the ";
-                            cout << "current value." << endl;
-                            std::getline(cin, new_setting);
-                            if (new_setting == "") {
-                                outfile << read_line << endl;
-                            } else {
-                                outfile << "[" << setting_name << "]" << "=" << new_setting << endl;
-                            }
-                            current_setting = "";
-                            new_setting + "";
-
-                        } else {
-                            outfile << read_line << endl;
-                        }
-
+                        outfile << read_line << endl;
                     }
+
                 }
             }
-
-            infile.close();
-            outfile.close();
-
-        } else {
-            cout << "Error backing up original config file";
         }
-    } else {
-        // invalid argument
-        cout << "Invalid argument us int or edit." << endl;
-        cout << "When using edit either enter a value to edit" << endl;
-        cout << "(name, email, cypher, timezone, or knownrecipients)." << endl;
-        cout << "If now setting is entered then the settings will be cycled through" << endl;
-    }
 
-    return 0;
+        infile.close();
+        outfile.close();
+
+    } else {
+        cout << "Error backing up original config file";
+    }
+}
+
+else {
+// invalid argument
+cout << "Invalid argument us int or edit." <<
+endl;
+cout << "When using edit either enter a value to edit" <<
+endl;
+cout << "(name, email, cypher, timezone, or knownrecipients)." <<
+endl;
+cout << "If no setting is entered then the settings will be cycled through" <<
+endl;
+}
+
+return 0;
 }
 
