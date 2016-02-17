8a269514a232a1c92d23414e804bde28ce01bcf5
Brandon Collins <bcseahawk@gmail.com>
2016-02-17 14:04:36 -0500

    argument changes

--- Files_Coding_Assignment_2/main.cpp
+++ Files_Coding_Assignment_2/main.cpp
@@ -9,68 +9,75 @@ using std::string;
 
 const string CONFIG_FILE = "C:/Users/Brandon/Documents/config.txt";
 
+void init() {
+    std::ofstream outfile;
+    outfile.open("config.txt");
+
+    //init user enter first name
+    string first_name = "";
+    do {
+        cout << "enter your first name:" << endl;
+        getline(cin, first_name);
+    } while (first_name == "");
+
+    //init user enter last name
+    string last_name;
+    do {
+        cout << "enter your last name" << endl;
+        getline(cin, last_name);
+    } while (last_name == "");
+
+    //init user enter email address
+    string email_address = "";
+    do {
+        cout << "enter your email address" << endl;
+        std::getline(cin, email_address);
+    } while (email_address == "");
+
+    //init user enter cypher
+    string cypher_passcode;
+    do {
+        cout << "enter your cypher: " << endl;
+        std::getline(cin, cypher_passcode);
+    } while (cypher_passcode == "");
+
+    //init user enter time offset
+    string time_offset = "";
+    do {
+        cout << "enter your time offset: " << endl;
+        std::getline(cin, time_offset);
+    } while (time_offset == "");
+
+    //init user enter known recipients file
+    string known_recipients_file = "";
+    do {
+        cout << "enter known recipients file: " << endl;
+        getline(cin, known_recipients_file);
+    } while (known_recipients_file == "");
+    cout << "known_recipients_file= knownrecipients.txt" << endl;
+
+    outfile.close();
+}
+
 int main(int argc, char *argv[]) {
 
-    void("init");
-    {
-        cout << argc;
-        if (static_cast<string>(argv[1]) == "init") {
-            cout << "init" << endl;
-        }
 
-
-        std::ofstream outfile;
-        outfile.open("config.txt");
-
-        //init user enter first name
-        string first_name = "";
-        do {
-            cout << "enter your first name:" << endl;
-            getline(cin, first_name);
-        } while (first_name == "");
-
-        //init user enter last name
-        string last_name;
-        do {
-            cout << "enter your last name" << endl;
-            getline(cin, last_name);
-        } while (last_name == "");
-
-        //init user enter email address
-        string email_address = "";
-        do {
-            cout << "enter your email address" << endl;
-            std::getline(cin, email_address);
-        } while (email_address == "");
-
-        //init user enter cypher
-        string cypher_passcode;
-        do {
-            cout << "enter your cypher: " << endl;
-            std::getline(cin, cypher_passcode);
-        } while (cypher_passcode == "");
-
-        //init user enter time offset
-        string time_offset = "";
-        do {
-            cout << "enter your time offset: " << endl;
-            std::getline(cin, time_offset);
-        } while (time_offset == "");
-
-        //init user enter known recipients file
-        string known_recipients_file = "";
-        do {
-            cout << "enter known recipients file: " << endl;
-            getline(cin, known_recipients_file);
-        } while (known_recipients_file == "");
-        cout << "known_recipients_file= knownrecipients.txt" << endl;
-
-        outfile.close();
+    cout << argc;
+    if (argc != 0) {
+        cout << "opening config file" << CONFIG_FILE << endl;
     }
+
+    if (static_cast<string>(argv[1]) == "init") {
+        cout << init;
+    }
+
     if (static_cast<string>(argv[2]) == "edit") {
-        cout << "edit" << endl;
+        cout << edit;
     }
+}
 
+
+void edit() {
     std::ifstream infile;
     infile.open("config.txt");
     cout << "Reading data from the file.\n";
@@ -107,11 +114,11 @@ int main(int argc, char *argv[]) {
     cout << "The information was saved to the file .\n" << endl;
 
     outfile.close();
+
     infile.close();
-
-
-    return 0;
 }
+
+
 
 
 
